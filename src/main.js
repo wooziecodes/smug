9cd21eae75701975932dd84bb5fe1fb996639a2e
choijuhwan8 <choijuhwan8@gmail.com>
2022-09-03 23:23:44 +0800

    update on firebase project

--- src/main.js
+++ src/main.js
@@ -6,15 +6,17 @@ import * as firebase from "firebase/app";
 // import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBfNtrggmhOCX3Z8Db-hZZmd5dvDqzUhSI",
-  authDomain: "smug-b36fc.firebaseapp.com",
-  projectId: "smug-b36fc",
-  storageBucket: "smug-b36fc.appspot.com",
-  messagingSenderId: "626641004276",
-  appId: "1:626641004276:web:344dcf15a6b2216ec6e0a1",
-  measurementId: "G-4D1CPFLCNZ"
-};
-
+    apiKey: "AIzaSyCIs-jCOTCgMspvCH3C3EKd_rszakumjyY",
+    authDomain: "smug-6af00.firebaseapp.com",
+    databaseURL: "https://smug-6af00-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "smug-6af00",
+    storageBucket: "smug-6af00.appspot.com",
+    messagingSenderId: "528889510749",
+    appId: "1:528889510749:web:f69e557a590d0f9add06b4",
+    measurementId: "G-EWRYTVZDC5"
+  };
+  
+  
 firebase.initializeApp(firebaseConfig);
 // const analytics = getAnalytics(app);
 createApp(App).use(store).use(router).mount('#app')
