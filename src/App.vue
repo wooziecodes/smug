1d47b70c0914cca5d49394789f401f1e8ff19359
choijuhwan8 <choijuhwan8@gmail.com>
2022-09-03 22:03:37 +0800

    restart

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,10 @@
 <template>
   <nav>
     <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link> |
-    <router-link to="/login">Login</router-link> |
-    <router-link to="/signup">SignUp</router-link>
+    <router-link to="/about">About</router-link>
   </nav>
   <router-view/>
 </template>
-
-<script>
-import { onBeforeMount } from 'vue';
-// import { firebase } from 'firebase';
-// import * as firebase from "firebase/app"
-import { useRouter, useRoute } from 'vue-router'
-
-export default{
-  // setup() {
-  //   const router = useRouter();
-  //   const route = useRoute();
-
-  //   onBeforeMount(() => {
-  //     firebase.auth().onAuthStateChanged((user) => {
-  //       if(!user) {
-  //         router.replace('/login');
-  //       } else if (route.path == "/login" || route.path == "/register") {
-  //         router.replace('/');
-  //       }
-  //     });
-  //   });
-  // },
-}
-</script>
-
-
 
 <style lang="scss">
 #app {
--- src/router/index.js
+++ src/router/index.js
@@ -1,43 +1,11 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
-import Login from '../views/Login.vue'
-<<<<<<< Updated upstream:smug/src/router/index.js
-import Register from '../views/Register.vue'
-import Secret from '../views/Secret.vue'
-=======
-import SignUp from '../views/SignUp.vue'
->>>>>>> Stashed changes:src/router/index.js
 
 const routes = [
   {
     path: '/',
     name: 'home',
     component: HomeView
-  },
-  {
-<<<<<<< Updated upstream:smug/src/router/index.js
-    path: '/Login',
-=======
-    path: '/login',
->>>>>>> Stashed changes:src/router/index.js
-    name: 'Login',
-    component: Login
-  },
-  {
-<<<<<<< Updated upstream:smug/src/router/index.js
-    path: '/Register',
-    name: 'Register',
-    component: Register
-  },
-  {
-    path: '/Secret',
-    name: 'Secret',
-    component: Secret
-=======
-    path: '/signup',
-    name: 'SignUp',
-    component: SignUp
->>>>>>> Stashed changes:src/router/index.js
   },
   {
     path: '/about',
