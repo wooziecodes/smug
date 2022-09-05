213e10e035c0206016ff27ea146ead21cb1846f4
choijuhwan8 <choijuhwan8@gmail.com>
2022-09-05 12:03:50 +0800

    .

---
+++ src/views/Login.vue
@@ -0,0 +1,47 @@
+<template>
+    <div>
+      <h1>Login</h1>
+      <form @submit.prevent="Login">
+      <input type="email" placeholder="Email" v-model="email">
+      <input type="password" placeholder="Password" v-model="password">
+      <input type="submit" value="Login">
+      <p>Don't have an account???<router-link to="/signup">Register Here</router-link></p>
+    </form>
+    </div>
+  </template>
+  
+  
+  <script>
+  // import firebase from "firebase";
+  // src='https://cdn.firebase.com/js/client/2.2.1/firebase.js';
+  import {ref} from 'vue';
+//   import * as firebase from "firebase";
+// v9 compat packages are API compatible with v8 code
+    import firebase from 'firebase/compat/app';
+    import 'firebase/compat/auth';
+    import 'firebase/compat/firestore';
+  
+  export default {
+      setup() {
+          const email = ref("");
+          const password = ref("");
+  
+          const Login = () => {
+          firebase
+              .auth()
+              .signInWithEmailAndPassword(email.value, password.value)
+              .then(data => console.log(data))
+              .catch(err => alert(err.message));
+          }
+          return {
+              Login,
+              email,
+              password
+          }
+      }
+  };
+  </script>
+  
+  <style>
+  
+  </style>
