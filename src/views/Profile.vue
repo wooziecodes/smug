c687b4693e10d8576c1a7abc5301ec2d4eaafe03
Wei Sheng Ang <aws1@hotmail.sg>
2022-11-10 16:47:38 +0800

    profile

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -1,18 +1,31 @@
 <template>
   <!-- <Navbar></Navbar> -->
 
-  
+
 
   <div class="container" id="profile" style="display:flex">
     
-    <div id="left-side"> 
+    <div id="left-side">
       <!-- this is 30% -->
-      <img :src="photo" id="photo">
-      <div id="rating" class="container text-center">2.5 
-          <font-awesome-icon icon="fa-solid fa-star" class="fa-star"/>
-          | 89 ratings
+  
+      <div class="profilePic">
+        <img :src="photo" id="photo">
+
+       <label v-if="editing" for="photo-upload" class="editPhoto" >
+        Edit Photo
+       </label>
+       <input type="file"
+       accept="image/png, image/jpeg" id="photo-upload" style="display:none" @change="changePhoto">
       </div>
-      <button type="button" class="btn" id="edit-button" style="background-color: fuchsia;">Edit</button>
+
+      <div id="rating" class="container text-center">2.5
+        <font-awesome-icon icon="fa-solid fa-star" class="fa-star" />
+        | 89 ratings
+      </div>
+      <button type="button" class="btn" id="edit-button" style="background-color: fuchsia;" v-if="!editing"
+        @click="toggleEdit()">Edit</button>
+      <button type="button" class="btn" id="edit-button" style="background-color: fuchsia;" v-if="editing"
+        @click="toggleEdit()">Save</button>
       <!-- ideally the button should be like the signup page but im fucking losing my shit because background-color doesnt wanna work with me but color works so i can change the color of the words-->
 
     </div>
@@ -20,36 +33,49 @@
     <div id="right-side">
       <h1 v-text="name" id="name"></h1>
       <table>
-          <!-- <tr>
+        <!-- <tr>
             <th>Name</th>
             <td>{{name}}</td>
           </tr> -->
-            
-          <tr>
-            <th>Faculty</th>
-            <td>{{faculty}}</td>
-          </tr>
-          <tr>
-            <th>Major</th>
-            <td>{{major}}</td>
-          </tr>
-          <tr>
-            <th>Year</th>
-            <td>{{year}}</td>
-          </tr>
-          <tr>
-            <th>Email</th>
-            <td>{{email}}</td>
-          </tr>
-          <tr>
-            <th>Payment method</th>
-            <td>{{payment}}</td>
-          </tr>
-          <tr>
-            <th>Description</th>
-            <td>{{description}}</td>
-          </tr>
-        </table>
+
+        <tr>
+          <th>Faculty</th>
+          <td v-if="!editing">{{ faculty }}</td>
+          <td v-if="editing"><input type="text" v-model="faculty"></td>
+        </tr>
+        <tr>
+          <th>Major</th>
+          <td v-if="!editing">{{ major }}</td>
+          <td v-if="editing"><input type="text" v-model="major"></td>
+        </tr>
+        <tr>
+          <th>Year</th>
+          <td v-if="!editing">{{ year }}</td>
+          <td v-if="editing"><input type="text" v-model="year"></td>
+        </tr>
+        <tr>
+          <th>Email</th>
+          <td v-if="!editing">{{ email }}</td>
+          <td v-if="editing"><input type="text" v-model="email"></td>
+        </tr>
+        <tr>
+          <th>Payment method</th>
+          <td v-if="!editing && payment==''">Unspecified</td> 
+          <td v-if="!editing">{{payment}}</td> 
+          <td v-if="editing">
+            <select v-model="payment">
+            <option>PayLah</option>
+            <option>PayNow</option>
+            <option>Cash</option>
+          </select></td>
+          
+        </tr>
+        <tr>
+          <th>Description</th>
+          <td v-if="!editing">{{ description }}</td>
+          <td v-if="editing"><textarea v-model="description" id="w3review" name="w3review" rows="8" cols="83"></textarea></td>
+        </tr>
+      </table>
 
 
 
@@ -58,7 +84,7 @@
   </div>
 
   <hr>
-    <!-- <div id="photo" class="container">
+  <!-- <div id="photo" class="container">
       <img
         src="https://randomuser.me/api/portraits/women/81.jpg"
         alt=""
@@ -66,11 +92,11 @@
       />
     </div> -->
 
-    <!-- <div id="userid" class="container text-center position-relative">
+  <!-- <div id="userid" class="container text-center position-relative">
       <h1>KrazyWoman1</h1>
     </div> -->
 
-    <!-- <div id="rating" class="container text-center">2.5 
+  <!-- <div id="rating" class="container text-center">2.5 
           <font-awesome-icon icon="fa-solid fa-star" class="fa-star"/>
           | 89 ratings
       </div>
@@ -103,11 +129,11 @@
           </tr>
         </table>
       </div> -->
-      <div class="listings-container container d-flex flex-wrap">
+  <div class="listings-container container d-flex flex-wrap">
     <ListingComponent v-for="listing in listings" class="listing-component" :tutor="listing.user" :code="listing.module"
       :prof="listing.prof" :price="listing.price" :userID="listing.userID"></ListingComponent>
   </div>
-      
+
 </template>
 
 <script>
@@ -121,15 +147,16 @@ export default {
     return {
       listings: [],
       name: "Chester Chia",
-      photo:"https://randomuser.me/api/portraits/women/81.jpg",
+      photo: "https://randomuser.me/api/portraits/women/81.jpg",
       stars: "2.5",
       rating: "89",
       faculty: "SCIS",
       major: "Business Analytics",
       year: 2,
       email: "chester@gods.com",
-      payment: "Paylah",
-      description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Odit enim fugit reprehenderit neque, tenetur totam libero maiores et atque placeat minus ex ducimus, sit nisi a sint mollitia laborum optio?"
+      payment: "",
+      description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Odit enim fugit reprehenderit neque, tenetur totam libero maiores et atque placeat minus ex ducimus, sit nisi a sint mollitia laborum optio?",
+      editing: false
     };
   },
   created() {
@@ -142,72 +169,110 @@ export default {
         this.listings.push(doc.data());
       });
     },
+    toggleEdit() {
+      if (this.editing) {
+        this.editing = false;
+      }
+      else {
+        this.editing = true;
+      }
+    },
+    changePhoto(event) {
+      console.log(event.target.files[0].name);
+   }
   },
-  components: { ListingComponent,  Navbar },
+  components: { ListingComponent, Navbar },
 };
 </script>
 <style>
 * {
-    margin:0;
-    padding:0;
+  margin: 0;
+  padding: 0;
 }
 
-#profile{
+#profile {
   margin-top: 5%;
   width: 80%;
 }
 
-#left-side{
+#left-side {
   width: 40%;
   margin-right: 2%
 }
 
 #photo {
-    width: 100%;
-    border-radius: 10%;
-    margin-bottom: 10px;
-    
+  width: 100%;
+  border-radius: 10%;
+  margin-bottom: 10px;
+
 }
+
 #userid {
-    margin-top: 20px;
+  margin-top: 20px;
 }
 
 #logo {
-    width: 166px;
-    height: 38px;
-    margin-left: 20px
+  width: 166px;
+  height: 38px;
+  margin-left: 20px
 }
 
 .desc {
-    margin-top: 20px;
-    margin-left: 340px;
-    margin-right: 240px;
-}
-th {
-    white-space: nowrap;
-    vertical-align: top;
-    text-align: right;
-    font-weight: bold;
-}
-td {
-    text-align: left;
-    padding-left: 10px;
-    width: fixed;
+  margin-top: 20px;
+  margin-left: 340px;
+  margin-right: 240px;
 }
 
-#name{
-  font-weight:bolder;
+th {
+  white-space: nowrap;
+  vertical-align: top;
+  text-align: right;
+  font-weight: bold;
+}
+
+td {
+  text-align: left;
+  padding-left: 10px;
+  width: fixed;
+}
+
+#name {
+  font-weight: bolder;
   color: #75ACB4
 }
 
-#edit-button{
+#edit-button {
   width: 100%;
   margin-top: 10px;
-  background-color: #1F5C64 !important; 
+  background-color: #1F5C64 !important;
 }
 
-#edit-button:hover{
+#edit-button:hover {
   color: red;
 }
+.profilePic {
+  position: relative;
+}
+.profilePic .editPhoto {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  -ms-transform: translate(-50%, -50%);
 
+  color: white;
+  font-size: 16px;
+  padding: 12px 24px;
+  border: none;
+  cursor: pointer;
+  border-radius: 5px;
+  text-align: center;
+  display: none;
+}
+
+.profilePic:hover .editPhoto {
+  display: block;
+  background-color: #1F5C64 !important;
+}
 </style>
+
