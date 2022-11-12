f1274f98097d4383f4f376dbf6396488a2c8880c
choijuhwan8 <choijuhwan8@gmail.com>
2022-11-12 16:47:04 +0800

    margin fix

--- src/views/Landing.vue
+++ src/views/Landing.vue
@@ -9,7 +9,7 @@
               <kinesis-container>
                 <kinesis-element :strength="50">
 
-                  <h3 class="header-sub-title" style="color:black; text-align: center">
+                  <h3 class="header-sub-title" style="color:black; text-align: center; margin-top: 50px;">
                     Supercharge your learning
                   </h3>
                 </kinesis-element>
